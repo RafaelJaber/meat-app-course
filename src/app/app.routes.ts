7cec166c59fe19702e043081daff82fea681cc8a
Rafael Jaber <rafael.jaber@hotmail.com>
2019-07-12 16:42:54 -0300

    Lazy Load com os componentes de order

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,6 @@ import {RestaurantsComponent} from './restaurants/restaurants.component';
 import {RestaurantDetailComponent} from './restaurant-detail/restaurant-detail.component';
 import {MenuComponent} from './restaurant-detail/menu/menu.component';
 import {ReviewsComponent} from './restaurant-detail/reviews/reviews.component';
-import {OrderComponent} from './order/order.component';
 import {OrderSumaryComponent} from './order-sumary/order-sumary.component';
 
 
@@ -19,7 +18,7 @@ export const ROUTES: Routes = [
       {path: 'reviews', component: ReviewsComponent}
     ]
   },
-  {path: 'order', component: OrderComponent},
+  {path: 'order', loadChildren: './order/order.module#OrderModule'},
   {path: 'order-summary', component: OrderSumaryComponent},
   {path: 'about', loadChildren: './about/about.module#AboutModule'},
 ];
