012499604406388ee5778bfcc4dd110ff6dd09e1
Rafael Jaber <rafael.jaber@hotmail.com>
2019-07-13 00:04:21 -0300

    Animação do Reviews

--- src/app/restaurant-detail/reviews/reviews.component.ts
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -4,12 +4,29 @@ import {Observable} from 'rxjs/Observable';
 import {ActivatedRoute} from '@angular/router';
 import {ReviewModel} from './review.model';
 
+import {trigger, state, style, animate, transition, keyframes} from '@angular/animations';
+
+
 @Component({
   selector: 'mt-reviews',
   templateUrl: './reviews.component.html',
-  styleUrls: ['./reviews.component.css']
+  styleUrls: ['./reviews.component.css'],
+  animations: [
+    trigger('reviewsAnimate', [
+      state('ready', style({
+        opacity: 1
+      })), transition('void => ready', [
+        style({
+          opacity: 0,
+          transform: 'scale(0) translateY(-100%)'
+        }), animate('250ms 0s ease-out')
+      ])
+    ])
+  ]
 })
 export class ReviewsComponent implements OnInit {
+
+  reviewsState = 'ready';
 
   reviews: Observable<ReviewModel[]>;
 
