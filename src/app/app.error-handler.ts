e6187db67a731c0e174309c6265276f5a7dcc5d9
Rafael Jaber <rafael.jaber@hotmail.com>
2019-07-16 13:46:32 -0300

    Correção das mensagens não ocultando, realizando a execussão dentro de uma zona

--- src/app/app.error-handler.ts
+++ src/app/app.error-handler.ts
@@ -1,5 +1,5 @@
 import {HttpErrorResponse} from '@angular/common/http';
-import {ErrorHandler, Injectable, Injector} from '@angular/core';
+import {ErrorHandler, Injectable, Injector, NgZone} from '@angular/core';
 import {NotificationService} from './core/notification.service';
 import {AuthService} from './core/auth.service';
 
@@ -10,23 +10,26 @@ export class ApplicationErrorHandler extends ErrorHandler {
   constructor(
     private notification: NotificationService,
     private injector: Injector,
+    private zone: NgZone
   ) {
     super()
   }
 
   handleError(errorResponse: HttpErrorResponse | any) {
     if (errorResponse instanceof HttpErrorResponse) {
-      switch (errorResponse.status) {
-        case 401:
-          this.injector.get(AuthService).handleLogin();
-          break;
-        case 403:
-          this.notification.notify('Acesso não autorizado.');
-          break;
-        case 404:
-          this.notification.notify('Recurso não encontrado, verifique sua internet');
-          break;
-      }
+      this.zone.run(() => {
+        switch (errorResponse.status) {
+          case 401:
+            this.injector.get(AuthService).handleLogin();
+            break;
+          case 403:
+            this.notification.notify('Acesso não autorizado.');
+            break;
+          case 404:
+            this.notification.notify('Recurso não encontrado, verifique sua internet');
+            break;
+        }
+      })
     }
     super.handleError(errorResponse)
   }
